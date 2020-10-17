f2c65125c7d9aef24ebdbfdcf639acaca78626b3
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-17 13:50:08 -0700

    can add books and form is set to favorite

--- fav_books/fav_books_app/models.py
+++ fav_books/fav_books_app/models.py
@@ -7,6 +7,6 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     user_who_posted = models.ForeignKey(User, related_name="messages", on_delete = models.CASCADE)
-    users_who_liked = models.ManyToManyField(User, related_name="books_liked")
+    users_who_favorited = models.ManyToManyField(User, related_name="books_favorited")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- fav_books/fav_books_app/views.py
+++ fav_books/fav_books_app/views.py
@@ -6,9 +6,20 @@ def index(request):
         if 'userid' in request.session:
             context ={
                 'authenticated_user': User.objects.get(id=request.session['userid']),
+                'allBooks':Book.objects.all()
             }
             print(request.session)
             print(request.session['userid'])
             return render(request, 'books.html', context)
         else:
             return HttpResponse("ERROR NOT SIGNED IN")
+
+def newBook(request):
+    if request.method =="POST":
+        Book.objects.create(
+            title = request.POST['title'],
+            description = request.POST['desc'],
+            user_who_posted = User.objects.get(id=request.session['userid']),
+
+        )
+        return redirect('/books')
