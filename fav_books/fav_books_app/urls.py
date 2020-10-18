69608597839955a5b64d51c6c356522e14026143
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-17 17:33:53 -0700

    delete and update are now working

--- fav_books/fav_books_app/urls.py
+++ fav_books/fav_books_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new', views.newBook),
-    path('addFav', views.favBook)
+    path('addFav', views.favBook),
+    path('<int:book_id>', views.showBook),
+    path('update', views.updateBook),
+    path('delete', views.deleteBook)
+
 ]
--- fav_books/fav_books_app/views.py
+++ fav_books/fav_books_app/views.py
@@ -29,3 +29,24 @@ def favBook(request):
         targetBook = Book.objects.get(id=request.POST['book_to_fav_id'])
         targetBook.users_who_favorited.add(User.objects.get(id=request.session['userid']))
         return redirect('/books')
+
+def showBook(request, book_id):
+    context = {
+        'authenticated_user': User.objects.get(id=request.session['userid']),
+        'selected_book': Book.objects.get(id=book_id)
+    }
+    return render(request, 'book_page.html', context)
+
+def updateBook(request):
+    selected_book = Book.objects.get(id=request.POST['book_id'])
+    print(selected_book)
+    selected_book.title = request.POST['title']
+    selected_book.description = request.POST['desc']
+    selected_book.save()
+    return redirect('/books/'+str(selected_book.id))
+
+def deleteBook(request):
+    
+    selected_book = Book.objects.get(id=request.POST['book_id'])
+    selected_book.delete()
+    return redirect('/books')
